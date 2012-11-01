b7994c2ee06036661a969f243a97c2fd7819c793
AlexNigl <alexnigl@gmail.com>
2012-11-02 00:49:19 +0100

    Proof of concept now with UDP instead of TCP

--- serverex1/src/p2p/Client.java
+++ serverex1/src/p2p/Client.java
@@ -1,52 +1,76 @@
 package p2p;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
 
 
 public class Client {
 	
 	
+	private String host;
+	private int port;
+	private DatagramSocket socket;
+	private InetSocketAddress server;
+	
+	
 	/**
 	 * Create a new instance of Client.
 	 * 
-	 * @param string
+	 * @param host Hostname or IP adress
+	 * @param port number
+	 * @throws SocketException if Socket can't be opened
 	 */
-	public Client(String host, int port) {
+	public Client(String host, int port) throws SocketException {
+		this.socket = new DatagramSocket(2345);
+		this.server = new InetSocketAddress(host, port);
+		this.sendPush("bar");
+		this.sendPush("foo");
+		this.sendPull();
+		this.sendPrint();
+	}
+	
+	private void send(String msg) {
+		byte[] string = msg.getBytes();
+		DatagramPacket packet;
 		try {
-			Socket client = new Socket(host, port);
-			BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream(), "utf-8"));
-			PrintWriter out = new PrintWriter(client.getOutputStream());
-			
-			out.write("push bar");
-			out.println();
-			out.flush();
-			out.write("push foo");
-			out.println();
-			out.flush();
-			out.write("pull");
-			out.println();
-			out.flush();
-			System.out.println(in.readLine());
-			out.write("print");
-			out.println();
-			out.flush();
-			out.close();
-			in.close();
-			client.close();
-		} catch (UnknownHostException e) {
+			packet = new DatagramPacket(string, string.length, this.server);
+			this.socket.send(packet);
+		} catch (SocketException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+	}
+	
+	private void sendPrint() {
+		this.send("print");
+	}
+	
+	private void sendPull() {
+		this.send("pull");
+		byte[] buffer = new byte[255];
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+		try {
+			this.socket.receive(packet);
+			byte[] answer = packet.getData();
+			System.out.println(new String(answer, 0, answer.length));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void sendPush(String string) {
+		this.send("push " + string);
 	}
 	
 	public static void main(String[] args) {
-		Client c = new Client("127.0.0.1", 1234);
+		try {
+			Client c = new Client("127.0.0.1", 1234);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- serverex1/src/p2p/Server.java
+++ serverex1/src/p2p/Server.java
@@ -1,12 +1,9 @@
 package p2p;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
 import java.util.Stack;
 
 
@@ -17,8 +14,8 @@ public class Server {
 	private static final String PUSH = "push";
 	private static final String PULL = "pull";
 	private static final String PRINT = "print";
-	private static final String EXIT = "exit";
 	Stack<String> stack;
+	private DatagramSocket s;
 	
 	
 	/**
@@ -27,46 +24,29 @@ public class Server {
 	public Server() {
 		this.stack = new Stack<String>();
 		try {
-			this.initializeSocket();
-		} catch (IOException e) {
+			this.s = new DatagramSocket(SERVER_PORT);
+		} catch (SocketException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	/**
-	 * @throws IOException
-	 */
-	private void initializeSocket() throws IOException {
-		ServerSocket s = new ServerSocket(SERVER_PORT);
-		Socket c;
+	public void startServer() {
+		byte[] buf;
+		DatagramPacket in;
 		while (true) {
+			buf = new byte[256];
+			in = new DatagramPacket(buf, buf.length);
 			try {
-				c = s.accept();
-				BufferedReader b = new BufferedReader(new InputStreamReader(c.getInputStream(), "utf-8"));
-				String in;
-				boolean cont = true;
-				while (cont) {
-					in = b.readLine();
-					cont = this.parse(c, in);
-				}
-				c.close();
+				this.s.receive(in);
+				this.parse(in);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
 	}
 	
-	/**
-	 * @param c
-	 * @param c
-	 * @return
-	 * @throws IOException
-	 */
-	private boolean parse(Socket c, String in) throws IOException {
-		if (in == null || c == null) {
-			return false;
-		}
+	private void parse(DatagramPacket packet) throws IOException {
+		String in = new String(packet.getData(), 0, packet.getLength());
 		System.out.println("in: " + in);
 		String[] parts = in.split(" ");
 		if (parts.length >= 1) {
@@ -74,25 +54,20 @@ public class Server {
 			if (command.equalsIgnoreCase(PUSH)) {
 				this.stack.push(in.substring(PUSH.length() + 1));
 			} else if (command.equalsIgnoreCase(PULL)) {
-				BufferedWriter b = new BufferedWriter(new OutputStreamWriter(c.getOutputStream(), "utf-8"));
 				if (this.stack.size() > 0) {
-					b.write(this.stack.pop());
-					b.newLine();
-					b.flush();
+					byte[] string = this.stack.pop().getBytes();
+					DatagramPacket answer = new DatagramPacket(string, string.length, packet.getSocketAddress());
+					this.s.send(answer);
 				}
 			} else if (command.equalsIgnoreCase(PRINT)) {
 				System.out.println(this.stack.toString());
-			} else if (command.equalsIgnoreCase(EXIT)) {
-				return false;
 			}
-			return true;
 		}
-		return false;
-		
 	}
 	
 	public static void main(String[] args) {
 		Server server = new Server();
+		server.startServer();
 	}
 	
 }
