73524809ec21d5cbd61b0b3718ed97b58e4d6a06
Younes Chahbi <younes.chahbi@sap.com>
2012-11-23 00:44:08 +0100

    first commit exo2
    Change-Id: I92a05026e0832deda11081a059a2d960ea670326

---
+++ peertopeerexo2/src/SynchStack.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class SynchStack extends Stack<String> {
+	public synchronized String pop() {
+		String resp = super.empty() ? "Stack empty" :super.pop() ;
+		return resp;
+
+	}
+
+	public synchronized String push(String item) {
+		return super.push(item);
+
+	}
+
+}
+
