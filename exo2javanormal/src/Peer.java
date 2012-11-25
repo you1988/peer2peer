6e72ca684fb8f2cf73ba4309602471c5f89934ef
Younes Chahbi <younes.chahbi@sap.com>
2012-11-25 22:08:02 +0100

    second commit
    Change-Id: I1a8e6f720ecb616e426f9102dd8cc654cd7115a4

--- exo2javanormal/src/Peer.java
+++ exo2javanormal/src/Peer.java
@@ -8,16 +8,22 @@ import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.net.SocketException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.Channel;
+import java.nio.channels.DatagramChannel;
+import java.nio.charset.Charset;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Peer {
-
+public class Peer extends Thread {
+	public static int numport = 1000;
+	public static UInterface uiInterface;
 	private static final String PUSH = "push";
 	private static final String PULL = "pull";
 	private static final String PRINT = "print";
 	private static final String ADD_PORT = "ADD_PORT";
-
+	private static final String DELET_PORT = "DELET";
 	private static final PrintStream out = System.out;
 	SynchStack stack;
 	DatagramSocket socket;
@@ -29,11 +35,12 @@ public class Peer {
 	 * 
 	 * @throws SocketException
 	 */
-	public void spawnNeighbours() throws SocketException {
+	public Peer spawnNeighbours() {
 		Peer neighbour = new Peer();
 		neighbour.portNeighbours.add(this.port);
-		neighbour.startServer();
+		neighbour.start();
 		portNeighbours.add(neighbour.port);
+		return neighbour;
 	}
 
 	/**
@@ -43,21 +50,35 @@ public class Peer {
 	 *            of the neighbour that will receive the list.
 	 */
 	public void sendNeighborsListToNeigh(int port) {
-		byte[] resp = null;
+		char[] resp = null;
 
 		SocketAddress neighbourAddress = new InetSocketAddress("localhost",
 				port);
-		for (byte b : resp) {
+		resp = new String("DELET " + this.port).toCharArray();
+		DatagramPacket answer = null;
+		try {
+//			answer = new DatagramPacket(resp, resp.length, neighbourAddress);
+//			this.socket.send(answer);
+			channel.send(Charset.forName("ascii").encode(
+					CharBuffer.wrap(resp)), neighbourAddress);
 
+				
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		for (int portNei : portNeighbours) {
 			if (portNei != port) {
-				resp = new String("ADD_PORT " + portNei).getBytes();
-				DatagramPacket answer = null;
+				resp = new String("ADD_PORT " + portNei).toCharArray();
+
 				try {
-					answer = new DatagramPacket(resp, resp.length,
-							neighbourAddress);
-					this.socket.send(answer);
+
+					channel.send(Charset.forName("ascii").encode(
+							CharBuffer.wrap(resp)), neighbourAddress);
+					//this.socket.send(answer);
 				} catch (SocketException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -69,50 +90,140 @@ public class Peer {
 		}
 	}
 
+	public static int generatePort() {
+		return numport++;
+	}
+
 	/**
 	 * stop the peer
+	 * @throws IOException 
 	 */
 
-	public void leaveMe() {
+	public void leaveMe() throws IOException {
 		for (int port : portNeighbours) {
+			uiInterface.removeConnection(this.port, port);
 			sendNeighborsListToNeigh(port);
 		}
 		// cut connection with all neighbours
-		this.socket.close();
+		uiInterface.removeVertex(this.port);
+		this.stop();
+		
 	}
-	
+
 	/**
 	 * Get the port of current peer
 	 */
-	public int getPort(){
+	public int getPort() {
 		return this.port;
 	}
 
 	/**
 	 * Create a new instance of Server.
 	 */
+	DatagramChannel channel ;
 	public Peer() {
 		this.stack = new SynchStack();
-		Random generator = new Random();
-		this.port = generator.nextInt();
+
+		this.portNeighbours = new ArrayList<Integer>();
+		int port = generatePort();
+		boolean isNotOK = true;
+		while(isNotOK){
+		try {
+			channel = DatagramChannel.open();
+			channel.bind(new InetSocketAddress("localhost", port));
+		isNotOK = false;
+		} catch (IOException e1) {
+			port = generatePort();
+		}
+		}
+		this.port = port;
+//		try {
+//			this.socket = new DatagramSocket(this.port);
+//		} catch (SocketException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	}
+public void addPort(int port){
+
+	int i = 0;
+	boolean notFinded = true;
+	while (notFinded && i < portNeighbours.size()) {
+		
+		if (port == portNeighbours.get(i)){
+			notFinded = false;
+		}
+i++;
+	}
+
+	if (notFinded){
+		System.out
+		.println("ME the port" + this.port
+				+ "I have received a new neighbour"
+				+ port);
+
+		portNeighbours.add(port);
+		uiInterface.addPeer(this.port, port);
+	}
+	
+}
+	public void removePort(int port) {
+		int i = 0;
+		boolean notFinded = true;
+		while (notFinded && i < portNeighbours.size()) {
+			if (port == portNeighbours.get(i)){
+				notFinded = false;	
+			}
+			i++;
+			
+		}
+
+		if (!notFinded) portNeighbours.remove(i-1);
 	}
 
 	public void startServer() {
-		try {
-			ExecutorService exec = Executors.newFixedThreadPool(100);
-			this.socket = new DatagramSocket(this.port);
-			while (true) {
-				byte[] buf = new byte[256];
-				DatagramPacket dp = new DatagramPacket(buf, buf.length);
-				this.socket.receive(dp);
-				Worker w = new Worker(dp);
-				exec.execute(w);
+//		try {
+//			ExecutorService exec = Executors.newFixedThreadPool(100);
+
+			while (!this.isInterrupted()) {
+				final ByteBuffer buffer = ByteBuffer.allocate(64*1028);
+				try {
+					final SocketAddress sender = channel.receive(buffer);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				buffer.flip();
+				
+				String message = Charset.forName("ascii").decode(buffer).toString();
+out.print(message);
+				//				byte[] buf = new byte[256];
+//				DatagramPacket dp = new DatagramPacket(buf, buf.length);
+
+//				this.socket.receive(dp);
+//				String in = new String(dp.getData(), 0, dp.getLength(), "utf-8");
+//				out.println("in: " + in);
+				String[] parts = message.split(" ");
+				if (parts.length >= 1) {
+					String command = parts[0];
+					if (command.equalsIgnoreCase("ADD_PORT")) {
+						int portAdded = Integer.valueOf(parts[1]);
+
+						
+						addPort(portAdded);
+						
+					} else if (command.equalsIgnoreCase(DELET_PORT)) {
+						int portDeleted = Integer.valueOf(parts[1]);
+						removePort(portDeleted);
+					}
+				}
 			}
-		} catch (SocketException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+			try {
+				this.channel.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	}
 
 	public static void main(String[] args) {
@@ -139,8 +250,13 @@ public class Peer {
 				if (parts.length >= 1) {
 					String command = parts[0];
 					if (command.equalsIgnoreCase("ADD_PORT")) {
-						portNeighbours.add(Integer.valueOf(in
-								.substring(ADD_PORT.length() + 1)));
+						int portAdded = Integer.valueOf(in.substring(ADD_PORT
+								.length() + 1));
+						System.out.println("I have received a new neighbour"
+								+ portAdded);
+
+						portNeighbours.add(portAdded);
+
 					}
 					if (command.equalsIgnoreCase(PUSH)) {
 						Peer.this.stack.push(in.substring(PUSH.length() + 1));
@@ -166,5 +282,17 @@ public class Peer {
 				e1.printStackTrace();
 			}
 		}
+	}
+
+	public String toString() {
+		return "" + port;
+
+	}
+
+	@Override
+	public void run() {
+
+		startServer();
+
 	}
 }
--- exo2javanormal/src/UInterface.java
+++ exo2javanormal/src/UInterface.java
@@ -34,36 +34,38 @@ public class UInterface extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private UndirectedSparseGraph<String, String> graph;
-	private BasicVisualizationServer<String,String> vue;
+	private BasicVisualizationServer<String, String> vue;
 	private JComboBox list;
 	private String[] data = {};
 
-	private UInterface(){
-		
+	private UInterface() {
+		Peer.uiInterface = this;
 		this.setLayout(new FlowLayout());
 		Container content = this.getContentPane();
 		content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
-		
-		this.graph = new UndirectedSparseGraph<String,String>();
+
+		this.graph = new UndirectedSparseGraph<String, String>();
 		Peer firstPeer = new Peer();
-		graph.addVertex(""+firstPeer.getPort());
-		this.data = append(this.data, ""+firstPeer.getPort());
+		graph.addVertex("" + firstPeer.getPort());
+		this.data = append(this.data, "" + firstPeer.getPort());
 		this.vue = construire();
-        this.add(this.vue);
-        
+		this.add(this.vue);
+
 		JPanel panelSelector = new JPanel();
-		panelSelector.setLayout(new BoxLayout(panelSelector,BoxLayout.Y_AXIS));
-		this.list = new JComboBox(this.data);
-		this.list.setPreferredSize(new Dimension(0,0));
+		panelSelector.setLayout(new BoxLayout(panelSelector, BoxLayout.Y_AXIS));
+		// changed
+		this.list = new JComboBox();
+		this.list.addItem(firstPeer);
+		this.list.setPreferredSize(new Dimension(0, 0));
 		panelSelector.add(this.list);
-        
-        JPanel panelButtons = new JPanel();
+
+		JPanel panelButtons = new JPanel();
 		panelButtons.setLayout(new FlowLayout());
 		JButton add_button = new JButton("Spawn");
-		add_button.addActionListener(new addListener(this, this.list.getSelectedItem()));
+		add_button.addActionListener(new addListener(this, this.list));
 		panelButtons.add(add_button);
 		JButton remove_button = new JButton("Leave");
-		remove_button.addActionListener(new removeListener(this));
+		remove_button.addActionListener(new removeListener(this, this.list));
 		panelButtons.add(remove_button);
 
 		content.add(this.vue);
@@ -75,35 +77,63 @@ public class UInterface extends JFrame {
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
-	public BasicVisualizationServer<String,String> construire() {
-		Layout <String,String> layout = new FRLayout(this.graph);
-		layout.setSize(new Dimension(300,300));
-		BasicVisualizationServer<String,String> vue = new BasicVisualizationServer<String,String>(layout);
-		vue.setPreferredSize(new Dimension(350,350));
-		vue.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
+
+	public BasicVisualizationServer<String, String> construire() {
+		Layout<String, String> layout = new FRLayout(this.graph);
+		layout.setSize(new Dimension(300, 300));
+		BasicVisualizationServer<String, String> vue = new BasicVisualizationServer<String, String>(
+				layout);
+		vue.setPreferredSize(new Dimension(350, 350));
+		vue.getRenderContext()
+				.setVertexLabelTransformer(new ToStringLabeller());
 		vue.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller());
 		vue.getRenderer().getVertexLabelRenderer().setPosition(Position.CNTR);
 		return vue;
 	}
-	
+
 	/**
 	 * add element to data list
-	 * @param arr : data list
-	 * @param element : element to add
+	 * 
+	 * @param arr
+	 *            : data list
+	 * @param element
+	 *            : element to add
 	 * @return the new data list
 	 */
 	static <T> T[] append(T[] arr, T element) {
-	    final int N = arr.length;
-	    arr = Arrays.copyOf(arr, N + 1);
-	    arr[N] = element;
-	    return arr;
+		final int N = arr.length;
+		arr = Arrays.copyOf(arr, N + 1);
+		arr[N] = element;
+		return arr;
 	}
-	
-	public void addPeer(int parentPort, int childPort){
-		this.graph.addEdge("", ""+parentPort, ""+childPort);
+
+	public void addPeer(int parentPort, Peer childPeer) {
+		int childPort = childPeer.getPort();
+		this.graph.addEdge(parentPort +"-" + childPort, "" + parentPort, "" + childPort);
+		this.list.addItem(childPeer);
+		this.repaint();
+		this.repaint();
 	}
-	
+public void removeVertex(int port){
+	this.graph.removeVertex(""+port);
+	this.list.removeItem(this.list.getSelectedItem());
+	this.repaint();
+	this.repaint();
+}
+	public void removeConnection(int parentPort, int childPeerPort){
+		this.graph.removeEdge(parentPort +"-" + childPeerPort);
+		this.graph.removeEdge(childPeerPort +"-" + parentPort);
+		this.repaint();
+		this.repaint();
+	}
+	public void addPeer(int parentPort, int childPeerPort) {
+
+		this.graph.addEdge(parentPort +"-" + childPeerPort, "" + parentPort, ""
+				+ childPeerPort);
+		this.repaint();
+		this.repaint();
+	}
+
 	/**
 	 * @param args
 	 */
@@ -116,34 +146,48 @@ public class UInterface extends JFrame {
 class addListener implements ActionListener {
 
 	UInterface ui;
-	int parentPort;
-	
-	public addListener(UInterface ui, Object itemSelected){
+	JComboBox portList;
+
+	public addListener(UInterface ui, Object itemSelected) {
 		this.ui = ui;
-		//this.parentPort = (int) itemSelected;
+		this.portList = (JComboBox) itemSelected;
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		Peer newPeer = new Peer();
-		this.ui.addPeer(this.parentPort, newPeer.getPort());
+		Peer peer = (Peer) this.portList.getSelectedItem();
+		Peer neighbour = peer.spawnNeighbours();
+		String parentPort = ""
+				+ ((Peer) this.portList.getSelectedItem()).getPort();
+
+		this.ui.addPeer(Integer.valueOf(parentPort), neighbour);
+		this.ui.repaint();
+		this.ui.repaint();
 	}
-	
+
 }
 
 class removeListener implements ActionListener {
 
 	UInterface ui;
-	
-	public removeListener(UInterface ui){
+	JComboBox portList;
+
+	public removeListener(UInterface ui, Object itemSelected) {
 		this.ui = ui;
+		this.portList = (JComboBox) itemSelected;
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		Peer peer = (Peer) this.portList.getSelectedItem();
+		try {
+			peer.leaveMe();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	}
-	
+
 }
